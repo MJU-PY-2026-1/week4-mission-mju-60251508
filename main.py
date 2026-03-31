5c0479d9b6e7ff42baf737f030e6ba7643a44457
60251508 <60251508@mju.ac.kr>
2026-03-31 01:45:23 +0000

    문예준 60251508 4차미션  완료

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-# 파일이름 :
-# 작 성 자 :
+# 파일이름 : main.py
+# 작 성 자 : mju-3-31
+# 도장깨기 맛집리스트
+
+#비어있는 리스트 만들기
+bucket_list = []
+restaurant1 = input('맛집 리스트 입력: ')
+bucket_list.append(restaurant1)
+
+restaurant2 = input('맛집 리스트 입력: ')
+bucket_list.append(restaurant2)
+
+restaurant3 = input('맛집 리스트 입력: ')
+bucket_list.append(restaurant3)
+
+print(bucket_list)
+
+vip_restaurant = input('맛집 리스트 추가: ')
+bucket_list.insert(0, vip_restaurant)
+
+print(bucket_list)
+
+visited_restaurant = input(' 도장깨기 : ')
+bucket_list.remove(visited_restaurant)
+
+print(bucket_list)
+
